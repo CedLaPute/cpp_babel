73a2ad047010de8aaa56bd96ba5e42073f7923e9
CedLaPute <cedric.montavon@epitech.eu>
2016-10-19 17:09:58 +0200

    main et headers version 1.0

--- Client/ClientBabel/ClientBabel/Common/Sources/main.cpp
+++ Client/ClientBabel/ClientBabel/Common/Sources/main.cpp
@@ -1,4 +1,26 @@
+#ifdef WIN32
+# include "../../Windows/Headers/Windows.h"
+#else
+# include "../../Unix/Headers/Unix.h"
+#endif
+
 int main()
 {
+
+	// Déclaration pointeur sur interface de connexion
+	// AConnection *absConnexion = NULL;
+
+#ifdef WIN32
+
+	// Assignation de absConnexion pour Windows
+	// absConnexion = new WinConnexion(); 
+
+#else
+
+	// Assignation de absConnexion pour Unix
+	// absConnexion = new LinConnexion();
+
+#endif
+
 	return (0);
 }
--- Server/BabelServer/BabelServer/Common/Sources/main.cpp
+++ Server/BabelServer/BabelServer/Common/Sources/main.cpp
@@ -1,4 +1,26 @@
+#ifdef WIN32
+# include "../../Windows/Headers/Windows.h"
+#else
+# include "../../Unix/Headers/Unix.h"
+#endif
+
 int main()
 {
+
+	// Déclaration du pointeur sur interface de connexion
+	// AConnexion *absConnexion = NULL;
+
+#ifdef WIN32
+
+	// Assignation de absConnexion pour Windows
+	// absConnexion = new WinConnexion();
+
+#else
+
+	// Assignation de absConnexion pour Linux
+	// absConnexion = new LinConnexion();
+
+#endif
+
 	return (0);
 }
